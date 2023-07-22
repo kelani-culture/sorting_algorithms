844494bd2ed1bd128a4da637678b5647fa2db965
Null <kelanidarasimi9@gmail.com>
2023-07-22 16:00:47 +0100

    fix chagne 0(n) O(n)

--- sort.h
+++ sort.h
@@ -1,6 +1,6 @@
 #ifndef SORT_H
 #define SORT_H
-#include <stdio.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -10,14 +10,12 @@
  */
 typedef struct listint_s
 {
-	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
+const int n;
+struct listint_s *prev;
+struct listint_s *next;
 } listint_t;
-
-void print_list(const listint_t *list);
-void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 #endif

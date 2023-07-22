9ae3a8b4f539f8fa930dd37d4304bc2926d20f77
Null <kelanidarasimi9@gmail.com>
2023-07-22 01:36:54 +0100

    bubbled

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,7 +10,7 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t temp, i, j;
-	/*int bubbled;*/
+	int bubbled;
 
 	if (size <= 0 || size == 1)
 		print_array(array, size);
@@ -19,7 +19,7 @@ void bubble_sort(int *array, size_t size)
 	{
 		for (i = 0; i < size; i++)
 		{
-			/*bubbled = 0;*/
+			bubbled = 0;
 			for (j = i + 1; j < size; j++)
 			{
 				if (array[j] < array[i])
@@ -27,12 +27,12 @@ void bubble_sort(int *array, size_t size)
 					temp = array[i];
 					array[i] = array[j];
 					array[j] = temp;
-					/*bubbled = 1;*/
+					bubbled = 1;
 					print_array(array, size);
 				}
 			}
-			/*if (bubbled == 0)
-				break;*/
+			if (bubbled == 0)
+				break;
 		}
 	}
 }

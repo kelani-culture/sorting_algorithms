59471301aa8912b8e14b9d9e6c1e2efa9cbc46a1
Null <kelanidarasimi9@gmail.com>
2023-07-22 12:12:51 +0100

    fix bubble

--- main.c
+++ main.c
@@ -18,23 +18,23 @@ int main(void)
     /* printf("\n"); */
     /* print_array(array, n); */
     /* puts("-------------------------------------------------"); */
-    int arry[] = {12, 3, 4, 5, 1, 6};
-    size_t a = sizeof(arry) / sizeof(arry[0]);
+    /* int arry[] = {12, 3, 4, 5, 1, 6}; */
+    /* size_t a = sizeof(arry) / sizeof(arry[0]); */
 
-    print_array(arry, a);
-    printf("\n");
-    bubble_sort(arry, a);
-    printf("\n");
-    print_array(arry, a);
-
-    /* puts("-------------------------------------------------"); */
-    /* int arr[] = {-1, -2, -3, -4, -5, -6, -7, -8, -9, -10, -11, 12, 13, 14}; */
-    /* size_t b = sizeof(arr) / sizeof(arr[0]); */
-
-    /* print_array(arr, b); */
+    /* print_array(arry, a); */
     /* printf("\n"); */
-    /* bubble_sort(arr, b); */
+    /* bubble_sort(arry, a); */
     /* printf("\n"); */
-    /* print_array(arr, b); */
+    /* print_array(arry, a); */
+
+    puts("-------------------------------------------------");
+    int arr[] = {-1, -2, -3, -4, -5, -6, -7, -8, -9, -10, -11, 12, 13, 14};
+    size_t b = sizeof(arr) / sizeof(arr[0]);
+
+    print_array(arr, b);
+    printf("\n");
+    bubble_sort(arr, b);
+    printf("\n");
+    print_array(arr, b);
     return (0);
 }

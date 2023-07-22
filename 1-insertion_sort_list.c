d64c99b9795ef368f0e9fc493dfe3266485d7dee
Null <kelanidarasimi9@gmail.com>
2023-07-22 21:15:57 +0100

    fix both compilation error and betty error:

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -3,25 +3,27 @@
 
 /**
  * swap_nodes - swap nodes in the list
+ * @list: pointer to list
  * @node1: first node
  * @node2: second node
  */
 
 void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
 {
-    if (node1->prev)
-        node1->prev->next = node2;
-    if (node2->next)
-        node2->next->prev = node1;
 
-    node1->next = node2->next;
-    node2->prev = node1->prev;
-    node1->prev = node2;
-    node2->next = node1;
+	if (node1->prev)
+		node1->prev->next = node2;
+	if (node2->next)
+		node2->next->prev = node1;
 
-    if (!node2->prev)
-        *list = node2;
-    print_list(*list);
+	node1->next = node2->next;
+	node2->prev = node1->prev;
+	node1->prev = node2;
+	node2->next = node1;
+
+	if (!node2->prev)
+		*list = node2;
+	print_list(*list);
 }
 
 /**
@@ -31,28 +33,26 @@ void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
 
 void insertion_sort_list(listint_t **list)
 {
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-        return; /* Empty or single-node list is already sorted*/
+	listint_t *key, *j, *temp;
+	listint_t *current = (*list)->next;
 
-    listint_t *current = (*list)->next;
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return; /* Empty or single-node list is already sorted*/
 
-    while (current != NULL)
-    {
-        listint_t *key = current;
-        listint_t *j = current->prev;
+	while (current != NULL)
+	{
+		key = current;
+		j = current->prev;
 
-        while (j != NULL && key->n < j->n)
-        {
-            swap_nodes(list, j, key);
-            j = key->prev;
-            /*Printing the list after each swap */
-            listint_t *temp = *list;
-            while (temp)
-            {
-                temp = temp->next;
-            }
-        }
-
-        current = current->next;
-    }
+		while (j != NULL && key->n < j->n)
+		{
+			swap_nodes(list, j, key);
+			j = key->prev;
+			/*Printing the list after each swap */
+			temp = *list;
+			while (temp)
+				temp = temp->next;
+		}
+		current = current->next;
+	}
 }

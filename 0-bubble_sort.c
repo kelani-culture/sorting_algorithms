1677e175d4c34572ccaf25f6cfd327e4a74f57f9
Null <kelanidarasimi9@gmail.com>
2023-07-22 16:04:36 +0100

    fix compilation error

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -20,7 +20,7 @@ void bubble_sort(int *array, size_t size)
 		}
 		while (bubbled == FALSE)
 		{
-			bubbled = TRUE
+			bubbled = TRUE;
 			for (i = 0; i < size - 1; i++)
 			{
 				if (array[i + 1] < array[i])
--- sort.h
+++ sort.h
@@ -1,6 +1,6 @@
 #ifndef SORT_H
 #define SORT_H
-
+#include <stdio.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -14,6 +14,8 @@ const int n;
 struct listint_s *prev;
 struct listint_s *next;
 } listint_t;
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);

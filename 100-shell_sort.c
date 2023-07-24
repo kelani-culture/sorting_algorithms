e6cce96cdd0ca4881b9efcbb6d71a8a73ce7bab7
Null <kelanidarasimi9@gmail.com>
2023-07-24 17:17:40 +0100

    implement shell sort using knuth sequence

---
+++ 100-shell_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * shell_sort - sort element in an array using the shell sort
+ *	algrithm
+ * @array: array to be sorted
+ * @size: size of the array
+ */
+
+void shell_sort(int *array, size_t size)
+{
+	int gap = 1, outer, vI, i, inner;
+
+	while (gap <= size / 3)
+		gap = gap * 3 + 1;
+
+	while (gap > 0)
+	{
+		for (outer = gap; outer < size; outer++)
+		{
+			vI = array[outer];
+			inner = outer;
+			while (inner > gap - 1 && array[inner - gap]
+					>= vI)
+			{
+				array[inner] = array[inner - gap];
+				inner -= gap;
+			}
+			array[inner] = vI;
+		}
+		gap = (gap - 1) / 3;
+		i++;
+		print_array(array, size);
+	}
+}
